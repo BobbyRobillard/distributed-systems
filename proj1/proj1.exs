e5a7862262c8e32c3dec29112ed1fc19bbc8402d
Bobby Robillard <robillard123@gmail.com>
2019-09-07 00:08:15 -0400

    Update proj1.exs

--- proj1/proj1.exs
+++ proj1/proj1.exs
@@ -1,8 +1,24 @@
-[lower, upper] = Enum.map(System.argv, &String.to_integer/1)
+defmodule Parallel do
+  def pmap(num_workers, func) do
+    1 .. num_workers
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
+  end
+end
 
-coordinator_pid = spawn(Proj1.Coordinator, :loop, [[], upper - lower])
-
-lower..upper |> Enum.each(fn number ->
-  worker_pid = spawn(Proj1.Worker, :loop, [])
-  send(worker_pid, {coordinator_pid, number})
+[lower, upper, num_workers] = Enum.map(System.argv, &String.to_integer/1)
+total_size = Enum.count(lower..upper)
+Parallel.pmap(num_workers, fn x ->
+  starting = trunc(lower + ((x - 1) / num_workers) * total_size)
+  ending = trunc(x / num_workers * total_size + lower - 1)
+  Enum.each(
+     starting .. ending,
+      fn y ->
+        # res =
+          Proj1.Worker.ranged_search(y)
+        # if ! Enum.empty?(res) do
+        #   IO.puts Enum.join([y] ++ res, " ")
+        # end
+      end
+  )
 end)
