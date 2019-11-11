2b751aade9b0cda294b2046e787d08b773a155e8
Bobby Robillard <robillard123@gmail.com>
2019-11-10 21:48:56 -0500

    Update Node.ex

--- proj4/lib/Node.ex
+++ proj4/lib/Node.ex
@@ -2,11 +2,22 @@ defmodule Proj4.Node do
   use GenServer
 
   # ----------------------------------------------------------------------------
-  # API
+  # Public API
   # ----------------------------------------------------------------------------
 
   def start_link(username) do
-    GenServer.start_link(__MODULE__, {id, %{username: username, tweets: [], followers: []}}, name: via_tuple(name))
+    GenServer.start_link(
+      __MODULE__,
+      {
+        id,
+        %{
+          username: username,
+          is_online: :false,
+          tweets: [],
+          followers: []
+        }
+      },
+      name: via_tuple(name))
   end
 
   def publish_tweet(username, tweet) do
@@ -17,13 +28,24 @@ defmodule Proj4.Node do
     GenServer.cast(via_tuple(username), {:follow_user, username, user_to_follow})
   end
 
-  def retweet(username, tweet_owner, tweet_id) do
-    tweet = Proj4.Node.get_tweet(tweet_owner, tweet_id)
-    GenServer.cast(via_tuple(username), {:retweet, tweet})
+  def retweet(username, tweet) do
+    GenServer.cast(via_tuple(username), {:publish_tweet, tweet})
   end
 
   def query_tweets(username, query) do
     GenServer.call(via_tuple(username), {:query_tweets, query})
+  end
+
+  # ----------------------------------------------------------------------------
+  # Internal API
+  # ----------------------------------------------------------------------------
+
+  defp receive_tweet(username, tweet) do
+    GenServer.cast(via_tuple(username), {:receive_tweet, tweet})
+  end
+
+  defp handle_follow_from_user(username, username_of_follower) do
+    GenServer.cast(via_tuple(username), {:handle_follow_from_user, user_to_follow})
   end
 
   # ----------------------------------------------------------------------------
@@ -36,16 +58,28 @@ defmodule Proj4.Node do
   end
 
   @impl GenServer
-  def handle_cast({:follow_user, username, user_to_follow}) do
+  def handle_cast({:follow_user, username_of_follower, user_to_follow}) do
     # Add follower to other user's list of followers
-    Proj4.Node.send(user_to_follow, :follow, username)
+    Proj4.Node.follow(user_to_follow, username_of_follower)
   end
 
   @impl GenServer
-  def handle_cast({:retweet, tweet}) do
-    # Publish tweet to all followers
-    Map.get(state, :followers)
-    |> Enum.each(fn follower -> Proj4.Node.send(follower, tweet) end)
+  def handle_cast({:receive_tweet, tweet}) do
+    # Convert tweet into a user-friendly format
+    user_friendly_tweet = "This is the user friendly tweet"
+
+    # Check if tweet is a retweet, updating user-friendly tweet as necessary
+    if Map.get(tweet, :owner_username) != Map.get(state, username) do
+      user_friendly_tweet = "This is retweeted userfriendly tweet"
+    end
+
+    # Check if the tweet should be seen immediatley or sent to the user's mailbox
+    if Map.get(state, is_online) == :true do
+      IO.puts "Received Tweet"
+    else
+      new_mailbox = Map.get(state, :mailbox) | [user_friendly_tweet]
+      Map.replace(state, :mailbox, new_mailbox)
+    end
   end
 
   @impl GenServer
@@ -57,14 +91,14 @@ defmodule Proj4.Node do
 
   @impl GenServer
   def handle_cast({:publish_tweet, tweet}) do
-    # Publish tweet to all followers
-    IO.puts "Publishing new tweet to followers."
     Map.get(state, :followers)
-    |> Enum.each(fn follower -> Proj4.Node.send(follower, tweet) end)
+    |> Enum.each(fn follower -> Proj4.Node.receive_tweet(follower, tweet) end)
 
-    # Store tweet in our tweets
-    tweets = Map.get(state, :tweets) | tweet
-    {:ok, {Map.put(state, :tweets, tweets)}}
+    # Store tweet in our tweets only if it's not a re-tweet
+    if Map.get(tweet, :owner_username) == Map.get(state, username) do
+      tweets = Map.get(state, :tweets) | tweet
+      Map.put(state, :tweets, tweets)
+    end
   end
 
   # ----------------------------------------------------------------------------
