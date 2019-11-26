a4146f89b0854d6f12b08983a3b24710818f352a
WillBAnders <willbanders@gmail.com>
2019-11-26 12:00:39 -0500

    Update engine to use one-line commands and separate executor.

--- proj4/lib/engine.ex
+++ proj4/lib/engine.ex
@@ -1,52 +1,48 @@
 defmodule Proj4.Engine do
 
   def main() do
-   IO.puts("Welcome to MEGA's Twitter Simulator!")
-   print_help_message()
-   receive_command()
- end
+    IO.puts("Welcome to MEGA's Twitter Simulator!")
+    process()
+  end
 
- @commands %{
-   "quit" => "Exits twitter simulator",
-   "register" => "Registers a twitter user",
-   "reteet" => "Send a tweet from someone you follow to your followers",
-   "tweet" => "Send out a tweet to your followers",
-   "tweets" => "See all the tweets beloning to a user"
- }
+  def process() do
+    case execute(IO.gets("> ") |> String.trim |> String.split(" ", parts: 2)) do
+      {:ok, message} ->
+        IO.puts(message)
+        process()
+      {:error, message} ->
+        IO.puts("Error processing command: #{message}")
+        process()
+      {:quit} -> nil
+    end
+  end
 
- def retweet(username, tweet) do
+  def execute(["quit"]), do: {:quit}
 
- end
+  def execute(["register", username]) do
+    Proj4.Supervisor.register_user(username)
+    {:ok, "User #{username} registered successfully."}
+  end
 
- def receive_command do
-   IO.gets("\n> ")
-   |> String.trim
-   |> String.downcase
-   |> execute_command
- end
+  def execute(["tweet", _message]) do
+    {:error, "Command 'tweet' not implemented."}
+  end
 
- def execute_command("quit"), do: IO.puts "Exiting twitter!"
+  def execute(["tweets", _username]) do
+    {:error, "Command 'tweets' not implemented."}
+  end
 
- def execute_command("register") do
-   username = IO.gets("\nEnter New Username> ") |> String.trim
-   Proj4.Supervisor.register_user(username)
-   IO.puts "User #{username} has been registered successfully"
-   receive_command()
- end
+  def execute(["help"]) do
+    {:ok, [
+            "quit: Exits program.",
+            "register <username>: Registers a Twitter user.",
+            "tweet <message>: Tweets a message.",
+            "tweets <username>: View a users tweets.",
+            "help: View available commands."
+          ] |> Enum.join("\n")
+    }
+  end
 
- def execute_command("retweet") do
-   # IO.gets("\nEnter New Username> ") |> String.trim
-   # Proj4.Supervisor.register_user(username)
-   # IO.puts "User #{username} has been registered successfully"
-   receive_command()
- end
-
- def execute_command(_invalid_command), do: print_help_message()
-
- defp print_help_message do
-   IO.puts("\nThe simulator supports following commands:\n")
-   @commands
-   |> Enum.map(fn({command, description}) -> IO.puts("  #{command} - #{description}") end)
- end
+  def execute(_), do: {:error, "Invalid command (run 'help' for commands)."}
 
 end
